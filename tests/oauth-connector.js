30869524ed63b291b876bcfbd606a541b6b0fa09
Yahya <yahya1608@gmail.com>
2021-07-17 13:34:38 +0530

    Token processing

--- tests/oauth-connector.js
+++ tests/oauth-connector.js
@@ -2,6 +2,8 @@ const Nocks = require("./test-helpers/nocks");
 const test = require('ava');
 const OAuthConnector = require('../lib/oauth-connector');
 const Enum = require('../lib/enum')
+const momment = require('moment')
+const _ = require('lodash')
 
 test.before(async t => {
 	t.context.nocks = new Nocks();
@@ -15,6 +17,20 @@ test.before(async t => {
     t.context.oAuthConnector = new OAuthConnector(t.context.configJson, t.context.dc);
 });
 
+function compareResposne(response, expectedResponse, t){
+    let responseExpiresIn = _.get(response, 'expires_in');
+    let expectedResponseExpiresIn = _.get(expectedResponse, 'expires_in');
+    if(_.isNumber(expectedResponseExpiresIn)){
+        expectedResponseExpiresIn = momment().add(expectedResponseExpiresIn, 'second').toISOString()
+    }
+    _.unset(response, 'expires_in');
+    _.unset(expectedResponse, 'expires_in')
+    t.deepEqual(response, expectedResponse)
+    let diff = momment(responseExpiresIn).diff(expectedResponseExpiresIn, 'minute')
+    t.is(Math.abs(diff) < 10, true)
+
+}
+
 test('getAuthorizationUrl', async t => {
     const oAuthConnector = t.context.oAuthConnector;
     let url = oAuthConnector.getAuthorizationUrl(Enum.AccessType.Offline, true, '123');
@@ -26,7 +42,7 @@ test.serial('getAccessTokenAndRefreshToken', async t => {
     const oAuthConnector = t.context.oAuthConnector;
 
     const expectedResponse = {
-        expires_in : new Date().getTime().toString(),
+        expires_in : 360,
         token_type : 'bearer',
         access_token : 'access-token-1',
         refresh_token : 'refresh-token-1'
@@ -36,14 +52,14 @@ test.serial('getAccessTokenAndRefreshToken', async t => {
     nocks.getAccessToken(expectedResponse);
 
     let response = await oAuthConnector.getAccessTokenAndRefreshToken('code-1-1-1', '123');
-    t.deepEqual(response, expectedResponse);
+    compareResposne(response, expectedResponse, t);
 });
 
 test.serial('getAccessTokenFromRefreshToken', async t => {
     const oAuthConnector = t.context.oAuthConnector;
     const refreshToken = 'refresh-token-2'
     const expectedResponse = {
-        expires_in : new Date().getTime().toString(),
+        expires_in : 360,
         token_type : 'bearer',
         access_token : 'access-token-1',
         refresh_token : refreshToken
@@ -53,14 +69,14 @@ test.serial('getAccessTokenFromRefreshToken', async t => {
     nocks.getAccessToken(expectedResponse);
 
     let response = await oAuthConnector.getAccessTokenFromRefreshToken(refreshToken);
-    t.deepEqual(response, expectedResponse);
+    compareResposne(response, expectedResponse, t);
 });
 
 test.serial('getValidAccessToken -> Valid time', async t => {
     const oAuthConnector = t.context.oAuthConnector;
     const accessToken = 'access-token-5'
     const tokenInfo = {
-        expires_in : (new Date().getTime() + 1000000).toString(),
+        expires_in : momment().add(60, 'minute').toISOString(),
         token_type : 'bearer',
         access_token : accessToken,
         refresh_token : 'refreshToken-1'
@@ -75,14 +91,14 @@ test.serial('getValidAccessToken -> Expired time', async t => {
     const oAuthConnector = t.context.oAuthConnector;
     const accessToken = 'access-token-5'
     const tokenInfo = {
-        expires_in : (new Date().getTime() - 1000000).toString(),
+        expires_in : momment().subtract(30, 'minute').toISOString(),
         token_type : 'bearer',
         access_token : accessToken,
         refresh_token : 'refreshToken-1'
     };
 
     const expectedResponse = {
-        expires_in : new Date().getTime().toString(),
+        expires_in : 360,
         token_type : 'bearer',
         access_token : 'access-token-10',
         refresh_token : 'refreshToken-10'
@@ -92,7 +108,7 @@ test.serial('getValidAccessToken -> Expired time', async t => {
 
 
     let response = await oAuthConnector.getValidTokenInfo(tokenInfo);
-    t.deepEqual(response.tokenInfo, expectedResponse);
+    compareResposne(response.tokenInfo, expectedResponse, t);
     t.deepEqual(response.isValid, false);
 });
 
@@ -110,8 +126,8 @@ test('getCodeFromRedirectedUrl -> Multiple query string', async t => {
 
 test('getCodeFromRedirectedUrl -> Multiple query string with url', async t => {
     const oAuthConnector = t.context.oAuthConnector;
-    let code = oAuthConnector.getCodeFromRedirectedUrl('https://localhost/redirect?code=1234&test=jshs');
-    t.deepEqual(code, '1234');
+    let code = oAuthConnector.getCodeFromRedirectedUrl('http://localhost/redirect?state=123&code=1000.aefa01c3644ad19b09e46c23ae04359d.a2b6eebc5c5c79d1661835dac574d725&location=us&accounts-server=https%3A%2F%2Faccounts.zoho.com&');
+    t.deepEqual(code, '1000.aefa01c3644ad19b09e46c23ae04359d.a2b6eebc5c5c79d1661835dac574d725');
 });
 
 
